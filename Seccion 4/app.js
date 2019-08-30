3d430217c26e9897bca80e0479fe414b7b62bd41
DamianVogel <rodolfovogel@gmail.com>
2019-08-30 17:25:11 -0300

    Cambios en ejercicio

--- Seccion 4/app.js
+++ Seccion 4/app.js
@@ -10,10 +10,34 @@ const argv = require('yargs')
                     }
                 })
                 .help()
-                .argv
+                .argv;
 
+
+//const argv = require('yargs');
 
 const { crearArchivo } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch(comando){
+
+    case 'listar':
+        console.log(comando);
+    break;
+
+    case 'crear':
+            crearArchivo(argv.base).then(respuesta =>{
+                console.log(`El archivo ${respuesta} ha sido creado exitosamente`);
+            }).catch(err =>{
+                console.log(err);
+            })
+    break;    
+
+    default:
+        console.log(`Comando ${comando} no reconocido`);
+    break;
+
+}
 
 
 //let base =  'asd';
@@ -23,11 +47,6 @@ const { crearArchivo } = require('./multiplicar/multiplicar');
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
 
-console.log(`El limite es: ${argv.limite}`);
+//console.log(`El limite es: ${argv.limite}`);
 //console.log(argv2);
 
-// crearArchivo(base).then(respuesta =>{
-//     console.log(`El archivo ${respuesta} ha sido creado exitosamente`);
-// }).catch(err =>{
-//     console.log(err);
-// })
--- Seccion 4/multiplicar/multiplicar.js
+++ Seccion 4/multiplicar/multiplicar.js
@@ -2,6 +2,37 @@ const fs = require('fs');
 
 let data = '';
 
+
+let listaTabla = (base, limite) =>{
+    
+    return new Promise ((resolve, reject)=>{
+        
+        if(!Number(base) || !Number(limite)){
+            reject(`La base ${base} o el ${limite} no es un numero`);
+            return
+        }
+        
+        for(let i = 1; i < limite ; i++){
+            console.log(`${i} * ${base} = ${i * base}`);
+        }
+        
+        //const data = new Uint8Array(Buffer.from('Hola Mundo'));
+        
+        // fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+          
+        //     if (err) 
+        //         reject(err);
+        //     else
+        //         resolve(`tabla-${base}.txt`);
+        
+        // });
+    })
+
+
+
+}
+
+
 let crearArchivo = (base)=>{
 
     return new Promise ((resolve, reject)=>{
@@ -30,7 +61,8 @@ let crearArchivo = (base)=>{
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listaTabla
 }
 
 //COMO UTILIZAR LA FUNCION EN UN ARCHIVO QUE ESTA EN OTRO LADO. 
