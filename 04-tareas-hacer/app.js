621a76d9f6d628668a50c1e6d5008a1c1155c9a5
Damián Vogel <rvogel@factorit.com.ar>
2021-04-07 22:57:05 -0300

    varios

---
+++ 04-tareas-hacer/app.js
@@ -0,0 +1,22 @@
+
+require('colors');
+const { mostrarMenu,pausa } = require('./helpers/mensajes');
+
+const main = async () => {
+    
+    console.clear();
+    //console.log('Hola Mundo');
+
+    mostrarMenu();
+
+    pausa();
+
+}
+
+
+
+main();
+
+
+
+
